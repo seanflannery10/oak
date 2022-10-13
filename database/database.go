b0ebb8290a2e0fd3b9f290774c13ea49be23034e
Sean Flannery <seanflannery10@gmail.com>
2022-10-12 21:43:59 -0400

    Changed to use connection pool

--- database/database.go
+++ database/database.go
@@ -2,55 +2,52 @@ package database
 
 import (
 	"context"
-	"database/sql"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"time"
-
-	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type Config struct {
-	dsn          string
-	maxOpenConns int
-	maxIdleConns int
-	maxIdleTime  string
-	maxLifetime  string
+	dsn                   string
+	maxConns              int32
+	maxConnLifetime       string
+	maxConnLifetimeJitter string
+	maxConnIdleTime       string
 }
 
-const defaultTimeout = 3 * time.Second
-
-func New(cfg Config) (*sql.DB, error) {
-
-	db, err := sql.Open("pgx", "postgres://"+cfg.dsn)
+func New(cfg Config) (*pgxpool.Pool, error) {
+	config, err := pgxpool.ParseConfig(cfg.dsn)
 	if err != nil {
 		return nil, err
 	}
 
-	db.SetMaxOpenConns(cfg.maxOpenConns)
-	db.SetMaxIdleConns(cfg.maxIdleConns)
-	db.SetMaxOpenConns(cfg.maxOpenConns)
-	db.SetMaxIdleConns(cfg.maxIdleConns)
+	config.MaxConns = cfg.maxConns
 
-	idleDuration, err := time.ParseDuration(cfg.maxIdleTime)
+	maxConnLifetime, err := time.ParseDuration(cfg.maxConnLifetime)
 	if err != nil {
 		return nil, err
 	}
 
-	db.SetConnMaxIdleTime(idleDuration)
+	config.MaxConnLifetime = maxConnLifetime
 
-	lifeDuration, err := time.ParseDuration(cfg.maxLifetime)
+	maxConnLifetimeJitter, err := time.ParseDuration(cfg.maxConnLifetimeJitter)
 	if err != nil {
 		return nil, err
 	}
 
-	db.SetConnMaxLifetime(lifeDuration)
+	config.MaxConnLifetimeJitter = maxConnLifetimeJitter
 
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
-	defer cancel()
-
-	err = db.PingContext(ctx)
+	maxConnIdleTime, err := time.ParseDuration(cfg.maxConnIdleTime)
 	if err != nil {
 		return nil, err
 	}
 
-	return db, nil
+	config.MaxConnIdleTime = maxConnIdleTime
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		return nil, err
+	}
+	defer pool.Close()
+
+	return pool, nil
 }
