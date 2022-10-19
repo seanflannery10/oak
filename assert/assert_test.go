1440e5828f2abbbb6b23d9e1bee5cb25dd773c91
Sean Flannery <seanflannery10@gmail.com>
2022-10-19 17:24:41 -0400

    Changed to floats

--- assert/assert_test.go
+++ assert/assert_test.go
@@ -5,28 +5,28 @@ import (
 	"testing"
 )
 
-func TestEqualInt(t *testing.T) {
-	tests := []struct {
-		a int
-		e int
+func TestEqual(t *testing.T) {
+	testsNums := []struct {
+		a float32
+		e float32
 		r bool
 	}{
 		{200, 200, true},
 		{201, 200, false},
+		{20.0, 20.0, true},
+		{20.1, 20.0, false},
 	}
-	for _, tt := range tests {
+
+	for _, tt := range testsNums {
 		t.Run(fmt.Sprintf("%#v", tt.a), func(t *testing.T) {
-			mockT := new(testing.T)
-			res := Equal(mockT, tt.a, tt.e)
+			res := Equal(new(testing.T), tt.a, tt.e)
 			if res != tt.r {
 				t.Errorf("Equal(%#v, %#v) should return %#v", tt.e, tt.a, tt.r)
 			}
 		})
 	}
-}
 
-func TestEqualString(t *testing.T) {
-	tests := []struct {
+	testsStrings := []struct {
 		a string
 		e string
 		r bool
@@ -34,10 +34,10 @@ func TestEqualString(t *testing.T) {
 		{"123", "123", true},
 		{"124", "123", false},
 	}
-	for _, tt := range tests {
+
+	for _, tt := range testsStrings {
 		t.Run(fmt.Sprintf("%#v", tt.a), func(t *testing.T) {
-			mockT := new(testing.T)
-			res := Equal(mockT, tt.a, tt.e)
+			res := Equal(new(testing.T), tt.a, tt.e)
 			if res != tt.r {
 				t.Errorf("Equal(%#v, %#v) should return %#v", tt.e, tt.a, tt.r)
 			}
