62b747a76f6f2f2f4985a2ce1d59c1006fdfee95
Sean Flannery <seanflannery10@gmail.com>
2022-10-21 13:07:30 -0400

    Added sig testing

--- server/server_test.go
+++ server/server_test.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"github.com/seanflannery10/oak/assert"
+	"os"
+	"syscall"
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
@@ -10,4 +13,44 @@ func TestNew(t *testing.T) {
 
 	assert.Equal(t, srv.Addr, "test")
 	assert.SameType(t, srv, &Server{})
+}
+
+func TestServer_Run(t *testing.T) {
+	t.Run("SIGINT", func(t *testing.T) {
+		srv := New("localhost:4444", nil)
+
+		go func() {
+			time.Sleep(500 * time.Millisecond)
+
+			p, err := os.FindProcess(os.Getpid())
+			if err != nil {
+				panic(err)
+			}
+			p.Signal(syscall.SIGINT)
+		}()
+
+		err := srv.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("SIGTERM", func(t *testing.T) {
+		srv := New("localhost:4444", nil)
+
+		go func() {
+			time.Sleep(500 * time.Millisecond)
+
+			p, err := os.FindProcess(os.Getpid())
+			if err != nil {
+				panic(err)
+			}
+			p.Signal(syscall.SIGTERM)
+		}()
+
+		err := srv.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
 }
