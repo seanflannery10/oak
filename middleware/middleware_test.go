92750c6840d4d739623365801696ad4c32e42cb5
Sean Flannery <seanflannery10@gmail.com>
2022-10-29 13:06:56 -0400

    Chagned test to use a loop

--- middleware/middleware_test.go
+++ middleware/middleware_test.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"github.com/seanflannery10/ossa/assert"
 	"github.com/seanflannery10/ossa/helpers"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -19,88 +20,89 @@ var (
 )
 
 func TestMiddleware_Authenticate(t *testing.T) {
-	t.Run("Missing Bearer", func(t *testing.T) {
-		rr := httptest.NewRecorder()
-		r, _ := http.NewRequest(http.MethodGet, "/", nil)
+	tests := []struct {
+		name   string
+		header string
+		jwks   string
+		apiURL string
+		body   string
+		code   int
+	}{
+		{
+			name:   "Missing Bearer",
+			header: `Test Header`,
+			jwks:   ``,
+			apiURL: "",
+			body:   "invalid or missing authentication token",
+			code:   http.StatusUnauthorized,
+		},
+		{
+			name:   "Bad Token",
+			header: `Bearer 1234`,
+			jwks:   ``,
+			apiURL: "",
+			body:   "invalid or missing authentication token",
+			code:   http.StatusUnauthorized,
+		},
+		{
+			name:   "Bad Audience",
+			header: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6ImF0K2p3dCIsImtpZCI6IkFzc0o3ZjByQ1M0eE5nbzE4cndkZDlXZEJ5RG96eFc0TnhhQjNDMXZaREkifQ.eyJyb2xlX25hbWVzIjpbXSwianRpIjoidkVEQ0luM3RlOXdscG9MN2ZhUW1PIiwic3ViIjoiWFZ1QXpLTkdZWjVxMzd2SmRmc0N3IiwiaWF0IjoxNjY2ODM3NzIwLCJleHAiOjEwMDAwMDAwMDE2NjY4Mzc4MDAsImNsaWVudF9pZCI6IlhWdUF6S05HWVo1cTM3dkpkZnNDdyIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMS9vaWRjIiwiYXVkIjoiaHR0cHM6Ly90ZXN0LmFwaSJ9.ffTskNjO-t9AFx6jpkmwqMfoY5DxCGJiVY3gjTRvasN6LahYWlZtFx7zaD9_MjJftpNzFAwJsqYmYLnB6GNvIM_oY_WUk_OP9_qGcTKvMtWYTRkI018q1zQLP_IpjQ3KVzGw_xIszbBXQ7wQrOKl28yN0UTQ-iraiPUVSVFQEfvoUZARc7zDtjlAHX-__fN5JoNMxQJfVdED_QDU8v9XbKI0ngwl99JwSeFxP9w8ByHmL3vgLXq8aQxODrfTZO-ev292Ziyy6iQWbLM6c-OnixYbPnugbpKF-d3rkIVsk6xx_cqyoAUnJc-Sz2un3ouqiLNMmK8IXqFnBW-fRMf6ApGDb3hTvH0wVxS4etJazH9Q0Np4CbB0O3B2dpi9gzHTqvNiTY6fPKrKYEvPKCVfewpp34r7-6hScfAEfluuz8y438OKWyANoOSwQ0ws58mh9F_3h59-sRYK23Mb_2yngmlJTvtY72RqzIEC617_lo0-8ABDqgr3ojJFrYbIhnYGxr2Wye715Ovu02Al3p580qGvqV3fCYtm7DUavfH7dyXQeS7Yb7YvejeIVrz-m_SRHO2FHYUDg268J9LGDpTVAPGPZVxD8jPMWNUeYCBtfPhlC0gTJI3By3qnq8hmke5gJTHonrzWTCHwkBcQUIdNzh1cAZe3uREzl7EbX2Hty5M`,
+			jwks:   `{"keys":[{"kty":"RSA","use":"sig","kid":"AssJ7f0rCS4xNgo18rwdd9WdByDozxW4NxaB3C1vZDI","e":"AQAB","n":"1DyGj6W-HjwAG4bwLlkWB9i8WLY3ILkFWpp5sCSMXptnJZ4JHpMEcIw4ecv4y2mfxBHvx52QTuh7_mzGILE3Aso-igyrqMtye8JrIFJ3P44m7i10MQ3si8yoUfaEZg2iiCyHTW2kPLspi9d1f7VY-aUTRlJavzxYd1KqvJmuxQ3fyxhJX-XAaW6HG2jDzrW0xSYSFB-lO56hHk1Hdr3JAd9Emwp4BoCsCqDv92dEMurfJEjv1O2hJRaHZ-cC_IFeuBDAQSnmjMRTytkVFQ8yUPUK3ozUIveZPoqk6KvBWjBMKHQjA1xS77MhcIfcmKFR-0tef2546TFRwWxAaVSubrzBy5MsD9f_sK7ntFLtJjk1ljfn1F8-eBMX3H2rd75xSWiAy6sUbNzFRfJiKRnzc1PTVMuHnSzcg_9HTv1Ve1F121w_ykRU316Nz8y9MpSSMuQUVCsOadi9Z9LL5iJkH7VTzSR3GJbOb5LnRuJa_9FChFn_P6hB_e7EbJ_ItVd36kCpezv4Iw_NdfFliQ3YZhAWr2ZDtPIgfLH6350LsYCz93wsLUVSRQLOhCsUWcD8EfOHURMiTMEs6hz4UOw603HFzHH93qTRVuPg6pKYLtZybnPZolr_j7Bpb-0h4chgKDa2rHz7LcUhz87eVXHExaKaQz9lLngqtkigslg4HOk"}]}`,
+			apiURL: "Test",
+			body:   "invalid or missing authentication token",
+			code:   http.StatusUnauthorized,
+		},
+		{
+			name:   "Bad Issuer",
+			header: `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6ImF0K2p3dCIsImtpZCI6IkFzc0o3ZjByQ1M0eE5nbzE4cndkZDlXZEJ5RG96eFc0TnhhQjNDMXZaREkifQ.eyJyb2xlX25hbWVzIjpbXSwianRpIjoidkVEQ0luM3RlOXdscG9MN2ZhUW1PIiwic3ViIjoiWFZ1QXpLTkdZWjVxMzd2SmRmc0N3IiwiaWF0IjoxNjY2ODM3NzIwLCJleHAiOjEwMDAwMDAwMDE2NjY4Mzc4MDAsImNsaWVudF9pZCI6IlhWdUF6S05HWVo1cTM3dkpkZnNDdyIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMS9vaWRjIiwiYXVkIjoiaHR0cHM6Ly90ZXN0LmFwaSJ9.ffTskNjO-t9AFx6jpkmwqMfoY5DxCGJiVY3gjTRvasN6LahYWlZtFx7zaD9_MjJftpNzFAwJsqYmYLnB6GNvIM_oY_WUk_OP9_qGcTKvMtWYTRkI018q1zQLP_IpjQ3KVzGw_xIszbBXQ7wQrOKl28yN0UTQ-iraiPUVSVFQEfvoUZARc7zDtjlAHX-__fN5JoNMxQJfVdED_QDU8v9XbKI0ngwl99JwSeFxP9w8ByHmL3vgLXq8aQxODrfTZO-ev292Ziyy6iQWbLM6c-OnixYbPnugbpKF-d3rkIVsk6xx_cqyoAUnJc-Sz2un3ouqiLNMmK8IXqFnBW-fRMf6ApGDb3hTvH0wVxS4etJazH9Q0Np4CbB0O3B2dpi9gzHTqvNiTY6fPKrKYEvPKCVfewpp34r7-6hScfAEfluuz8y438OKWyANoOSwQ0ws58mh9F_3h59-sRYK23Mb_2yngmlJTvtY72RqzIEC617_lo0-8ABDqgr3ojJFrYbIhnYGxr2Wye715Ovu02Al3p580qGvqV3fCYtm7DUavfH7dyXQeS7Yb7YvejeIVrz-m_SRHO2FHYUDg268J9LGDpTVAPGPZVxD8jPMWNUeYCBtfPhlC0gTJI3By3qnq8hmke5gJTHonrzWTCHwkBcQUIdNzh1cAZe3uREzl7EbX2Hty5M`,
+			jwks:   `{"keys":[{"kty":"RSA","use":"sig","kid":"AssJ7f0rCS4xNgo18rwdd9WdByDozxW4NxaB3C1vZDI","e":"AQAB","n":"1DyGj6W-HjwAG4bwLlkWB9i8WLY3ILkFWpp5sCSMXptnJZ4JHpMEcIw4ecv4y2mfxBHvx52QTuh7_mzGILE3Aso-igyrqMtye8JrIFJ3P44m7i10MQ3si8yoUfaEZg2iiCyHTW2kPLspi9d1f7VY-aUTRlJavzxYd1KqvJmuxQ3fyxhJX-XAaW6HG2jDzrW0xSYSFB-lO56hHk1Hdr3JAd9Emwp4BoCsCqDv92dEMurfJEjv1O2hJRaHZ-cC_IFeuBDAQSnmjMRTytkVFQ8yUPUK3ozUIveZPoqk6KvBWjBMKHQjA1xS77MhcIfcmKFR-0tef2546TFRwWxAaVSubrzBy5MsD9f_sK7ntFLtJjk1ljfn1F8-eBMX3H2rd75xSWiAy6sUbNzFRfJiKRnzc1PTVMuHnSzcg_9HTv1Ve1F121w_ykRU316Nz8y9MpSSMuQUVCsOadi9Z9LL5iJkH7VTzSR3GJbOb5LnRuJa_9FChFn_P6hB_e7EbJ_ItVd36kCpezv4Iw_NdfFliQ3YZhAWr2ZDtPIgfLH6350LsYCz93wsLUVSRQLOhCsUWcD8EfOHURMiTMEs6hz4UOw603HFzHH93qTRVuPg6pKYLtZybnPZolr_j7Bpb-0h4chgKDa2rHz7LcUhz87eVXHExaKaQz9lLngqtkigslg4HOk"}]}`,
+			apiURL: "https://test.api",
+			body:   "invalid or missing authentication token",
+			code:   http.StatusUnauthorized,
+		},
+		// TODO Add working good token
+		//{
+		//	name:   "Good Token",
+		//	header: `Bearer `,
+		//	jwks:   ``,
+		//	apiURL: "https://test.api",
+		//	body:   "OK",
+		//	code:   http.StatusOK,
+		//},
+	}
 
-		r.Header.Add("Authorization", "Test Header")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l, _ := net.Listen("tcp", "127.0.0.1:50000")
 
-		m.Authenticate(next).ServeHTTP(rr, r)
+			srv := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(tt.jwks))
+			}))
 
-		res := rr.Result()
-		body := helpers.GetBody(t, res)
+			srv.Listener.Close()
+			srv.Listener = l
 
-		assert.Equal(t, res.Header.Get("Vary"), "Authorization")
-		assert.Contains(t, body, "invalid or missing authentication token")
-		assert.Equal(t, res.StatusCode, http.StatusUnauthorized)
-	})
+			srv.Start()
+			defer srv.Close()
 
-	t.Run("Bad Token", func(t *testing.T) {
-		rr := httptest.NewRecorder()
-		r, _ := http.NewRequest(http.MethodGet, "/", nil)
+			m.SetAuthenticateConfig(srv.URL+"/oidc/jwks", tt.apiURL)
 
-		r.Header.Add("Authorization", "Bearer 1234")
+			rr := httptest.NewRecorder()
+			r, _ := http.NewRequest(http.MethodGet, "/oidc/jwks", nil)
 
-		m.Authenticate(next).ServeHTTP(rr, r)
+			r.Header.Add("Authorization", tt.header)
 
-		res := rr.Result()
-		body := helpers.GetBody(t, res)
+			m.Authenticate(next).ServeHTTP(rr, r)
 
-		assert.Contains(t, body, "invalid or missing authentication token")
-		assert.Equal(t, res.StatusCode, http.StatusUnauthorized)
-	})
+			res := rr.Result()
+			body := helpers.GetBody(t, res)
 
-	t.Run("Good Token, Bad Audience", func(t *testing.T) {
-		const testJWKS = `{"keys":[{"kty":"RSA","use":"sig","kid":"AssJ7f0rCS4xNgo18rwdd9WdByDozxW4NxaB3C1vZDI","e":"AQAB","n":"1DyGj6W-HjwAG4bwLlkWB9i8WLY3ILkFWpp5sCSMXptnJZ4JHpMEcIw4ecv4y2mfxBHvx52QTuh7_mzGILE3Aso-igyrqMtye8JrIFJ3P44m7i10MQ3si8yoUfaEZg2iiCyHTW2kPLspi9d1f7VY-aUTRlJavzxYd1KqvJmuxQ3fyxhJX-XAaW6HG2jDzrW0xSYSFB-lO56hHk1Hdr3JAd9Emwp4BoCsCqDv92dEMurfJEjv1O2hJRaHZ-cC_IFeuBDAQSnmjMRTytkVFQ8yUPUK3ozUIveZPoqk6KvBWjBMKHQjA1xS77MhcIfcmKFR-0tef2546TFRwWxAaVSubrzBy5MsD9f_sK7ntFLtJjk1ljfn1F8-eBMX3H2rd75xSWiAy6sUbNzFRfJiKRnzc1PTVMuHnSzcg_9HTv1Ve1F121w_ykRU316Nz8y9MpSSMuQUVCsOadi9Z9LL5iJkH7VTzSR3GJbOb5LnRuJa_9FChFn_P6hB_e7EbJ_ItVd36kCpezv4Iw_NdfFliQ3YZhAWr2ZDtPIgfLH6350LsYCz93wsLUVSRQLOhCsUWcD8EfOHURMiTMEs6hz4UOw603HFzHH93qTRVuPg6pKYLtZybnPZolr_j7Bpb-0h4chgKDa2rHz7LcUhz87eVXHExaKaQz9lLngqtkigslg4HOk"}]}`
-		const testHeader = `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6ImF0K2p3dCIsImtpZCI6IkFzc0o3ZjByQ1M0eE5nbzE4cndkZDlXZEJ5RG96eFc0TnhhQjNDMXZaREkifQ.eyJyb2xlX25hbWVzIjpbXSwianRpIjoidkVEQ0luM3RlOXdscG9MN2ZhUW1PIiwic3ViIjoiWFZ1QXpLTkdZWjVxMzd2SmRmc0N3IiwiaWF0IjoxNjY2ODM3NzIwLCJleHAiOjEwMDAwMDAwMDE2NjY4Mzc4MDAsImNsaWVudF9pZCI6IlhWdUF6S05HWVo1cTM3dkpkZnNDdyIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMS9vaWRjIiwiYXVkIjoiaHR0cHM6Ly90ZXN0LmFwaSJ9.ffTskNjO-t9AFx6jpkmwqMfoY5DxCGJiVY3gjTRvasN6LahYWlZtFx7zaD9_MjJftpNzFAwJsqYmYLnB6GNvIM_oY_WUk_OP9_qGcTKvMtWYTRkI018q1zQLP_IpjQ3KVzGw_xIszbBXQ7wQrOKl28yN0UTQ-iraiPUVSVFQEfvoUZARc7zDtjlAHX-__fN5JoNMxQJfVdED_QDU8v9XbKI0ngwl99JwSeFxP9w8ByHmL3vgLXq8aQxODrfTZO-ev292Ziyy6iQWbLM6c-OnixYbPnugbpKF-d3rkIVsk6xx_cqyoAUnJc-Sz2un3ouqiLNMmK8IXqFnBW-fRMf6ApGDb3hTvH0wVxS4etJazH9Q0Np4CbB0O3B2dpi9gzHTqvNiTY6fPKrKYEvPKCVfewpp34r7-6hScfAEfluuz8y438OKWyANoOSwQ0ws58mh9F_3h59-sRYK23Mb_2yngmlJTvtY72RqzIEC617_lo0-8ABDqgr3ojJFrYbIhnYGxr2Wye715Ovu02Al3p580qGvqV3fCYtm7DUavfH7dyXQeS7Yb7YvejeIVrz-m_SRHO2FHYUDg268J9LGDpTVAPGPZVxD8jPMWNUeYCBtfPhlC0gTJI3By3qnq8hmke5gJTHonrzWTCHwkBcQUIdNzh1cAZe3uREzl7EbX2Hty5M`
-
-		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(testJWKS))
-		}))
-		defer srv.Close()
-
-		m.SetAuthenticateConfig(srv.URL+"/oidc/jwksURL", "test")
-
-		rr := httptest.NewRecorder()
-		r, _ := http.NewRequest(http.MethodGet, "/oidc/jwks", nil)
-
-		r.Header.Add("Authorization", testHeader)
-
-		m.Authenticate(next).ServeHTTP(rr, r)
-
-		res := rr.Result()
-		body := helpers.GetBody(t, res)
-
-		assert.Contains(t, body, "invalid or missing authentication token")
-		assert.Equal(t, res.StatusCode, http.StatusUnauthorized)
-	})
-
-	t.Run("Good Token, Bad Issuer", func(t *testing.T) {
-		const testJWKS = `{"keys":[{"kty":"RSA","use":"sig","kid":"AssJ7f0rCS4xNgo18rwdd9WdByDozxW4NxaB3C1vZDI","e":"AQAB","n":"1DyGj6W-HjwAG4bwLlkWB9i8WLY3ILkFWpp5sCSMXptnJZ4JHpMEcIw4ecv4y2mfxBHvx52QTuh7_mzGILE3Aso-igyrqMtye8JrIFJ3P44m7i10MQ3si8yoUfaEZg2iiCyHTW2kPLspi9d1f7VY-aUTRlJavzxYd1KqvJmuxQ3fyxhJX-XAaW6HG2jDzrW0xSYSFB-lO56hHk1Hdr3JAd9Emwp4BoCsCqDv92dEMurfJEjv1O2hJRaHZ-cC_IFeuBDAQSnmjMRTytkVFQ8yUPUK3ozUIveZPoqk6KvBWjBMKHQjA1xS77MhcIfcmKFR-0tef2546TFRwWxAaVSubrzBy5MsD9f_sK7ntFLtJjk1ljfn1F8-eBMX3H2rd75xSWiAy6sUbNzFRfJiKRnzc1PTVMuHnSzcg_9HTv1Ve1F121w_ykRU316Nz8y9MpSSMuQUVCsOadi9Z9LL5iJkH7VTzSR3GJbOb5LnRuJa_9FChFn_P6hB_e7EbJ_ItVd36kCpezv4Iw_NdfFliQ3YZhAWr2ZDtPIgfLH6350LsYCz93wsLUVSRQLOhCsUWcD8EfOHURMiTMEs6hz4UOw603HFzHH93qTRVuPg6pKYLtZybnPZolr_j7Bpb-0h4chgKDa2rHz7LcUhz87eVXHExaKaQz9lLngqtkigslg4HOk"}]}`
-		const testHeader = `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6ImF0K2p3dCIsImtpZCI6IkFzc0o3ZjByQ1M0eE5nbzE4cndkZDlXZEJ5RG96eFc0TnhhQjNDMXZaREkifQ.eyJyb2xlX25hbWVzIjpbXSwianRpIjoidkVEQ0luM3RlOXdscG9MN2ZhUW1PIiwic3ViIjoiWFZ1QXpLTkdZWjVxMzd2SmRmc0N3IiwiaWF0IjoxNjY2ODM3NzIwLCJleHAiOjEwMDAwMDAwMDE2NjY4Mzc4MDAsImNsaWVudF9pZCI6IlhWdUF6S05HWVo1cTM3dkpkZnNDdyIsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMS9vaWRjIiwiYXVkIjoiaHR0cHM6Ly90ZXN0LmFwaSJ9.ffTskNjO-t9AFx6jpkmwqMfoY5DxCGJiVY3gjTRvasN6LahYWlZtFx7zaD9_MjJftpNzFAwJsqYmYLnB6GNvIM_oY_WUk_OP9_qGcTKvMtWYTRkI018q1zQLP_IpjQ3KVzGw_xIszbBXQ7wQrOKl28yN0UTQ-iraiPUVSVFQEfvoUZARc7zDtjlAHX-__fN5JoNMxQJfVdED_QDU8v9XbKI0ngwl99JwSeFxP9w8ByHmL3vgLXq8aQxODrfTZO-ev292Ziyy6iQWbLM6c-OnixYbPnugbpKF-d3rkIVsk6xx_cqyoAUnJc-Sz2un3ouqiLNMmK8IXqFnBW-fRMf6ApGDb3hTvH0wVxS4etJazH9Q0Np4CbB0O3B2dpi9gzHTqvNiTY6fPKrKYEvPKCVfewpp34r7-6hScfAEfluuz8y438OKWyANoOSwQ0ws58mh9F_3h59-sRYK23Mb_2yngmlJTvtY72RqzIEC617_lo0-8ABDqgr3ojJFrYbIhnYGxr2Wye715Ovu02Al3p580qGvqV3fCYtm7DUavfH7dyXQeS7Yb7YvejeIVrz-m_SRHO2FHYUDg268J9LGDpTVAPGPZVxD8jPMWNUeYCBtfPhlC0gTJI3By3qnq8hmke5gJTHonrzWTCHwkBcQUIdNzh1cAZe3uREzl7EbX2Hty5M`
-
-		srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(testJWKS))
-		}))
-		defer srv.Close()
-
-		m.SetAuthenticateConfig(srv.URL+"/oidc/jwks", "https://test.api")
-
-		rr := httptest.NewRecorder()
-		r, _ := http.NewRequest(http.MethodGet, "/oidc/jwks", nil)
-
-		r.Header.Add("Authorization", testHeader)
-
-		m.Authenticate(next).ServeHTTP(rr, r)
-
-		res := rr.Result()
-		body := helpers.GetBody(t, res)
-
-		assert.Contains(t, body, "invalid or missing authentication token")
-		assert.Equal(t, res.StatusCode, http.StatusUnauthorized)
-	})
+			assert.Equal(t, res.Header.Get("Vary"), "Authorization")
+			assert.Contains(t, body, tt.body)
+			assert.Equal(t, res.StatusCode, tt.code)
+		})
+	}
 }
 
 func TestMiddleware_CORS(t *testing.T) {
